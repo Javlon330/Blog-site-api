82158790be812a8c68e924041965affa024278c2
Javlon330 <rjavlonj6@gmail.com>
2022-03-12 18:26:03 +0500

    changes

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("github")
+const express = require('express')
+const dotenv = require('dotenv')
+const app = express()
+const mongoose = require('mongoose');
+dotenv.config()
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URL);
+
+app.get('/', (req, res) => {
+    res.json("This is blog-site")
+})
+
+const port = process.env.PORT || 5050
+
+app.listen(port, () => {
+    console.log(`Server listening port - ${port}`)
+})
