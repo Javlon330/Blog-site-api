d4d3f7b053cbbb2ee6bf89d1fba80232f5189ae7
Javlon330 <rjavlonj6@gmail.com>
2022-03-13 21:36:58 +0500

    c

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express')
 const dotenv = require('dotenv')
 const app = express()
 const mongoose = require('mongoose');
+const morgan = require('morgan')
+const multer = require('multer')
 const authRoute = require('./routes/auth')
 const userRoute = require('./routes/users')
+const postRoute = require('./routes/posts')
+const categoryRoute = require('./routes/categories')
 dotenv.config()
+
 app.use(express.json())
+app.use(morgan('tiny'))
 
 mongoose.connect(process.env.MONGO_URL_LOCAL)
     .then(() => {
@@ -15,8 +21,23 @@ mongoose.connect(process.env.MONGO_URL_LOCAL)
         console.log(err)
     })
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "images")
+    },
+    filename: (req, file, cb) => {
+        cb(null, "nature1.jpg")
+    }
+})
+const upload = multer({storage:storage});
+app.post("/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded")
+})
+
 app.use('/api/auth', authRoute)
 app.use('/api/users', userRoute)
+app.use('/api/posts', postRoute)
+app.use('/api/categories', categoryRoute)
 
 const port = process.env.PORT || 5050
 
